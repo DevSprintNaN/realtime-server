0ed447e15db27f542c5623bfd7cfc277b0e5a027
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2024-02-17 20:10:06 +0600

    Realtime Server Configuration Socket.io

--- server.js
+++ server.js
@@ -1,44 +1,89 @@
-require('dotenv').config()
+const { Server } = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+const axios = require('axios');
+const { createAdapter } = require('@socket.io/redis-adapter');
+const redis = require('redis');
+require('dotenv').config();
 
-const io = require('socket.io')(process.env._PORT, {
-    cors: {
-      origin: ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:8080', 'https://dev--dev-server-projecthub.netlify.app', 'https://production-projecthub.netlify.app']
+const app = express();
+const server = Server(app);
+const io = socketIO(server);
+
+const redisClient = redis.createClient({
+  host: process.env._REDIS_HOST,
+  port: process.env._REDIS_PORT,
+  password: process.env._REDIS_PASSWORD,
+});
+
+const subClient = redisClient.duplicate();
+io.adapter(createAdapter(redisClient, subClient));
+
+// Error handlers
+redisClient.on('error', err => {
+  console.error('Redis client error:', err);
+});
+
+subClient.on('error', err => {
+  console.error('Redis subClient error:', err);
+});
+
+const socketToRoom = {};
+
+io.on('connection', socket => {
+  console.log(`Socket ${socket.id} connected`);
+
+  socket.on('join room', roomName => {
+    console.log(`${socket.id} joined room`, roomName);
+    socket.join(roomName);
+    socketToRoom[socket.id] = roomName;
+  });
+
+  socket.on('leave room', roomName => {
+    socket.leave(roomName);
+    delete socketToRoom[socket.id];
+  });
+
+  socket.on('message', async message => {
+    const roomName = socketToRoom[socket.id];
+    if (roomName) {
+      io.to(roomName).emit('message', message);
+      console.log(`Message sent to room ${roomName}:`, message);
+      try {
+        await axios.post(`${process.env.PRIMARY_SERVER}/messaging/add`, {
+          ...message,
+          projectID: roomName
+        });
+      } catch (error) {
+        console.log("Server Not Available");
+      }
+    } else {
+      console.log(`Error: ${socket.id} is not in any room.`);
     }
   });
-  
-  const socketToRoom = {}; 
-  
-  io.on('connection', (socket) => {
-  
-    socket.on('join room', (roomName) => {
-      console.log(` ${socket.id} joined room`, roomName);
-      socket.join(roomName);
-      socketToRoom[socket.id] = roomName;
-    });
-  
-    socket.on('leave room', (roomName) => {
-      socket.leave(roomName);
-      delete socketToRoom[socket.id]; 
-    });
-  
-    socket.on('message', (message) => {
-      const roomName = socketToRoom[socket.id];
-      if (roomName) {
-        io.to(roomName).emit('message', message); 
-        console.log(`Message sent to room ${roomName}:`, message);
-      } else {
-        console.log(`Error: ${socket.id} is not in any room.`);
-      }
-    });
-  
-    socket.on('disconnect', () => {
-      const roomName = socketToRoom[socket.id];
-      if (roomName) {
-        console.log(`Socket ${socket.id} disconnected from room ${roomName}`);
-        delete socketToRoom[socket.id]; // Remove socket from room mapping when disconnecting
-      } else {
-        console.log(`Socket ${socket.id} disconnected`);
-      }
-    });
+
+  socket.on('canvas-data', data => {
+    const roomName = socketToRoom[socket.id];
+    if (roomName) {
+      io.to(roomName).emit('canvas-data', data);
+      console.log(`Canvas Data to room ${roomName}:`, data);
+    } else {
+      console.log(`Error: ${socket.id} is not in any room.`);
+    }
   });
-  
+
+  socket.on('disconnect', () => {
+    const roomName = socketToRoom[socket.id];
+    if (roomName) {
+      console.log(`Socket ${socket.id} disconnected from room ${roomName}`);
+      delete socketToRoom[socket.id];
+    } else {
+      console.log(`Socket ${socket.id} disconnected`);
+    }
+  });
+});
+
+const _PORT = process.env._PORT || 4000;
+server.listen(_PORT, () => {
+  console.log(`Server is running on port ${_PORT}`);
+});
